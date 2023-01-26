51175391cbb963c245b530dc92f18388e13cbcbe
Krishna Hendra Wijaya <46667254+KrishnaHendra@users.noreply.github.com>
2023-01-26 17:22:00 +0700

    feat: add auth validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash; 
+use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
